6f6d77cd24836f4591b7b2fd4c2e4379cd2cb63a
BernardBarnieh <barniehbernard@gmail.com>
2026-04-18 20:51:04 +0200

    Set up Node API portfolio template

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5050;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "API is running" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
